6b7c7e6ebe66a5fdf64c51aaeb61fc52f92faab2
Sitichai1995 <98759009+Sitichai1995@users.noreply.github.com>
2023-10-14 17:01:14 +0700

    update app file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import List from './pages/list';
+import MovieDetail from './pages/detail';
+import { Routes, Route } from "react-router-dom";
+import Cart from './pages/cart';
+import { ShoppingContextProvider } from './context/shoppingContext';
+import Payment from './pages/payment';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ShoppingContextProvider>
+      <Routes>
+      <Route path="/" element={<List />} />
+      <Route path="/movie/:movieId" element={< MovieDetail/>} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/payment" element={<Payment />} />
+      </Routes>
+    </ShoppingContextProvider>
+    
   );
 }
 
